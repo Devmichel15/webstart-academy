6d26565a9ab0f4e062a9a902fc6849e6282e8127
Michel César <httpmichel15cesar@gmail.com>
2026-09-19 01:20:25 +0100

    feat(pwa): add manifest and offline caching

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { HelmetProvider } from 'react-helmet-async'
-import 'devicon/devicon.min.css'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
+import "devicon/devicon.min.css";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+if (import.meta.env.PROD) {
+  import("virtual:pwa-register").then(({ registerSW }) =>
+    registerSW({ immediate: true }),
+  );
+}
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <HelmetProvider>
       <App />
     </HelmetProvider>
   </StrictMode>,
-)
+);
